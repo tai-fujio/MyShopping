5fda4e774ae3ece2c63100898af2d43013b84a18
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-19 08:59:51 +0900

    ItemBean実装

--- src/shopping/ItemBean.java
+++ src/shopping/ItemBean.java
@@ -1,5 +1,41 @@
 package shopping;
 
-public class ItemBean {
+import java.io.Serializable;
+
+public class ItemBean implements Serializable {
+
+	private String id;
+	private String name;
+	private int price;
+
+	public ItemBean() {
+		this.id = "";
+		this.name = "";
+		this.price = 0;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 }
