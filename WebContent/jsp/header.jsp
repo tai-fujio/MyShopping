8b31b2a3858a75bb7ec6ee163b89d4c4741914c1
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-18 19:13:51 +0900

    ログイン画面Servlet実装

--- WebContent/jsp/header.jsp
+++ WebContent/jsp/header.jsp
@@ -1,8 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<jsp:useBean id="user" scope="session" class="shopping.LoginUserBean"/>
+<jsp:useBean id="user" scope="session" class="shopping.UserBean"/>
 <div align=right>
 	ようこそ<jsp:getProperty property="name" name="user">さん
-	<a href="/shopping/BoughtListServlet">購入履歴</a>
+	<a href="/shopping/BoughtListServlet">購入</a>
 	<a href="/shopping/LoginServlet">ログアウト</a>
 </div>
--- src/shopping/LoginServlet.java
+++ src/shopping/LoginServlet.java
@@ -1,11 +1,14 @@
 package shopping;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,19 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,9 +37,36 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String btn = request.getParameter("submit");
+		HttpSession session = request.getSession();
+		RequestDispatcher rd;
+
+		if ("ログイン".equals(btn)) {
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			User user = new User();
+			UserBean userbean = user.getUserData(id, password);
+
+			if (userbean != null) {
+				session.setAttribute("user", userbean);
+				session.setAttribute("isLogged", true);
+				rd = request.getRequestDispatcher("./shopping/ShoppingListServlet");
+			} else {
+				rd = request.getRequestDispatcher("./jsp/loginNg.jsp");
+			}
+			rd.forward(request, response);
+		} else if ("ログアウト".equals(btn)) {
+			session.removeAttribute("isLogged");
+			session.removeAttribute("user");
+			response.sendRedirect("./jsp/login.jsp");
+
+		} else if ("購入一覧".equals(btn)) {
+			rd = request.getRequestDispatcher("./shopping/BaughtListServlet");
+		}
+
 	}
 
 }
