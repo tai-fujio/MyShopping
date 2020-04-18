0a31639fcf9bfc9cb24db960338b4e781d4f7d75
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-19 08:59:40 +0900

    StockBean実装

--- src/shopping/StockBean.java
+++ src/shopping/StockBean.java
@@ -1,5 +1,31 @@
 package shopping;
 
-public class StockBean {
+import java.io.Serializable;
+
+public class StockBean implements Serializable {
+
+	private String item_id;
+	private int quantity;
+
+	public StockBean() {
+		this.item_id = "";
+		this.quantity = 0;
+	}
+
+	public String getItem_id() {
+		return item_id;
+	}
+
+	public void setItem_id(String item_id) {
+		this.item_id = item_id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
 }
