dd2cc536828540682a54b47e1afd7cd3fcffb425
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-18 19:39:02 +0900

    BaughtListServlet実装

--- src/shopping/BaughtListServlet.java
+++ src/shopping/BaughtListServlet.java
@@ -1,11 +1,15 @@
 package shopping;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,19 +17,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class BaughtListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BaughtListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BaughtListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,9 +38,19 @@ public class BaughtListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		RequestDispatcher rd;
+		String btn = request.getParameter("submit");
+		if ("購入履歴".equals(btn)) {
+			Item item = new Item();
+			UserBean userbean = (UserBean) session.getAttribute("user");
+			ArrayList<HistoryBean> historybean = item.getHistoryData(userbean.getId());
+			request.setAttribute("history", historybean);
+			rd = request.getRequestDispatcher("./jsp/history.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 }
