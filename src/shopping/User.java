7d3110d0e959b8de980950f9471aef0a2001d201
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-19 10:40:38 +0900

    UserDao制御のためのUserクラス作成

--- src/shopping/User.java
+++ src/shopping/User.java
@@ -1,5 +1,24 @@
 package shopping;
 
+import java.sql.SQLException;
+
+import com.mysql.cj.protocol.Resultset;
+
 public class User {
+
+	public UserBean getUserData(String name, String password) {
+		UserDao dao = null;
+		Resultset result = null;
+
+		try {
+			dao = new UserDao();
+			result = dao.selectUser(name, password);
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} finally {
+			dao.close();
+		}
+		return result;
+	}
 
 }
