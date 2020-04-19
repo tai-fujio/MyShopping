282248ba238d6ba791b28e784f7e1b66e5dfe19c
tai-fujio <taisaburo.fujio@gmail.com>
2020-04-19 13:14:05 +0900

    ItemBeanにquantityフィールド追加

--- src/shopping/ItemBean.java
+++ src/shopping/ItemBean.java
@@ -7,11 +7,13 @@ public class ItemBean implements Serializable {
 	private String id;
 	private String name;
 	private int price;
+	private int quantity;
 
 	public ItemBean() {
 		this.id = "";
 		this.name = "";
 		this.price = 0;
+		this.quantity = 0;
 	}
 
 	public String getId() {
@@ -36,6 +38,14 @@ public class ItemBean implements Serializable {
 
 	public void setPrice(int price) {
 		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 
 }
